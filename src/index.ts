5cf46ed02880640e2bcbfc154f2eefde4f38a0dd
Adolphus Miracle <miraboy13@gmail.com>
2024-01-26 12:10:16 +0100

    feat: connected to mongoDb

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-console.log("Hello Typescript!");
+import express from 'express'
+import http from 'http';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+
+const app = express()
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app);
+
+dotenv.config()
+
+server.listen(8080, ()=>{
+    console.log("Server running on http://localhost:8080/");
+    
+})
+
+const MONGO_URL = process.env.MONGO_URL
+
+console.log(MONGO_URL);
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error)=>{
+    console.log(error);
+    
+})
