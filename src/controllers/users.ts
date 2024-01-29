313392c6f5c979ee2bbd654b9b16bc4a83bb483a
Adolphus Miracle <miraboy13@gmail.com>
2024-01-29 21:22:06 +0100

    feat: implemented logic to update users details

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import {deleteUserById, getUsers} from '../db/users';
+import {deleteUserById, getUsers, updateUserById} from '../db/users';
 
 export const getAllUsers =async (req: express.Request, res: express.Response,) => {
     try {
@@ -26,6 +26,28 @@ export const deleteUser =async (req: express.Request, res: express.Response) => 
 
 
         return res.json(deleteUser);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+        
+    }
+}
+
+export const updateUser =async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.params;
+        const {username} = req.body    
+        
+        const user = await updateUserById(id, username);
+        
+        if (!user) {
+            return res.sendStatus(403);
+        }
+
+        user.username = username;
+        await user.save();
+
+        return res.json(user);
     } catch (error) {
         console.log(error);
         return res.sendStatus(400);
